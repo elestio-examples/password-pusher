8ff10ce8fb69a51be5110ca9941dee12ecd03e7a
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-11-16 10:56:49 +0200

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,3 +1,7 @@
 #set env vars
 set -o allexport; source .env; set +o allexport;
 
+cat << EOT >> ./.env
+
+PWP__MAIL__MAILER_SENDER='"Password Pusher" <link-u353.vm.elestio.app@vm.elestio.app>'
+EOT
