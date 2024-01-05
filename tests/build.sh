feac38eaba909c0da65b835f1482c7c2bd1d267b
Prem chavhan <pchavhan@logiquad.com>
2024-01-05 12:27:06 +0530

    fix

--- tests/build.sh
+++ tests/build.sh
@@ -1,4 +1,4 @@
-cp containers/docker/pwpush/Dockerfile ./
+cp containers/docker/Dockerfile ./
 mv docker-compose-new.yml docker-compose.yml
 
 docker buildx build . --output type=docker,name=elestio4test/password-pusher:latest | docker load
